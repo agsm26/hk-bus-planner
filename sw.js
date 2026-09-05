e4ca7e2b02425e1d047bb7742d1d0b04cf647880
agsm26 <306416957+agsm26@users.noreply.github.com>
2026-09-06 00:07:04 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -43,7 +43,7 @@
    halfway through reading.
    ========================================================================= */
 
-const VERSION = 'hkfast-2026-09-04a';
+const VERSION = 'hkfast-2026-09-05a';
 const CACHE   = VERSION;
 
 // Fetched at install, before the app is ever called ready. Kept small on
